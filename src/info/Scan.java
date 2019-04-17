7dafb4abecd3a121e87446e2555f66ac16164a5b
an7360da-s <an7360da-s@student.lu.se>
2019-04-17 13:10:18 +0200

    Scan nästan klar

--- src/info/Scan.java
+++ src/info/Scan.java
@@ -1,9 +1,34 @@
 package info;
+import group09.Calculations;
 import robocode.ScannedRobotEvent;
 
 public class Scan {
 	
 	
+	public void onScannedRobot(ScannedRobotEvent e){
+		
+		EnemyRobot en = (EnemyRobot)Environment.enemies.get(e.getName());
+		 
+		if(en == null){
+			en = new EnemyRobot();
+			Environment.enemies.put(e.getName(), en);
+		}
+		
+		en.setName(e.getName());
+		en.setEnergy((double) e.getEnergy());
+		en.setAlive(true);
+		en.setPosition(Calculations.calcPoint(Robot.getPos(), e.getDistance(), getHeadingRadians() + e.getBearingRadians())); 
+ 
+		// normal target selection: the one closer to you is the most dangerous so attack him
+		if(!Robot.getTarget().getAlive() || e.getDistance() < Robot.getPos().distance(Robot.getTarget().getPosition())) {
+			Robot.setTarget(en);
+			
+		}
+ 
+		// locks the radar if there is only one opponent left
+		if(getOthers()==1)	setTurnRadarLeftRadians(getRadarTurnRemainingRadians());
+		
+	}
 	
 
 }
