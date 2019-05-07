8d4a98b635727064eb298731d4c380718e4ed2ac
an7360da-s <an7360da-s@student.lu.se>
2019-05-07 19:44:23 +0200

    EnemyRobotTest.java går igenom helt

--- src/tests/EnemyRobotTest.java
+++ src/tests/EnemyRobotTest.java
@@ -2,88 +2,126 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.geom.Point2D;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import info.EnemyRobot;
+
 public class EnemyRobotTest {
+	
+	private EnemyRobot testBot;
+	
+	private Point2D.Double position;
+	private Point2D.Double position2;
+
 
 	@Before
 	public void setUp() throws Exception {
+		testBot = new EnemyRobot();
+		testBot.setEnergy(200.0);
+		testBot.setHeading(180);
+		testBot.setVelocity(90);
+		testBot.setDistance(100);
+		testBot.setName("name1");
+		testBot.setAlive(false);
+		position = new Point2D.Double(100 ,100);
+		position2 = new Point2D.Double(200 ,200);
+		testBot.setPosition(position);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
 	public void testGetEnergy() {
-		fail("Not yet implemented");
+		
+		assertEquals("Failure", 200.0, testBot.getEnergy(), 0);
+		
 	}
 
 	@Test
 	public void testSetEnergy() {
-		fail("Not yet implemented");
+		
+		testBot.setEnergy(100);
+		assertEquals("Failure", 100 ,testBot.getEnergy(), 0);
 	}
 
 	@Test
 	public void testGetHeading() {
-		fail("Not yet implemented");
+		assertEquals("Failure", 180, testBot.getHeading(), 0);
+		
 	}
 
 	@Test
 	public void testSetHeading() {
-		fail("Not yet implemented");
+		testBot.setHeading(90);
+		assertEquals("Failure", 90, testBot.getHeading(), 0);
 	}
 
 	@Test
 	public void testGetVelocity() {
-		fail("Not yet implemented");
+		assertEquals("Failure", 90, testBot.getVelocity(), 0);
 	}
 
 	@Test
 	public void testSetVelocity() {
-		fail("Not yet implemented");
+		testBot.setHeading(50);
+		assertEquals("Failure", 50, testBot.getHeading(), 0);
+		
 	}
 
 	@Test
 	public void testGetDistance() {
-		fail("Not yet implemented");
+
+		assertEquals("Failure", 100, testBot.getDistance(), 0);
+	
 	}
 
 	@Test
 	public void testSetDistance() {
-		fail("Not yet implemented");
+		testBot.setDistance(50);
+		assertEquals("Failure", 50, testBot.getDistance(), 0);
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		assertEquals("Failure", "name1" , testBot.getName());
 	}
 
 	@Test
 	public void testSetName() {
-		fail("Not yet implemented");
+		testBot.setName("name2");
+		assertEquals("Failure", "name2", testBot.getName());
 	}
 
 	@Test
 	public void testGetAlive() {
-		fail("Not yet implemented");
+		assertEquals("Failure", false, testBot.getAlive());
 	}
 
 	@Test
 	public void testSetAlive() {
-		fail("Not yet implemented");
+		testBot.setAlive(true);
+		assertEquals("Failure", true , testBot.getAlive());
 	}
 
 	@Test
 	public void testGetPosition() {
-		fail("Not yet implemented");
+		assertEquals("Failure", 100, testBot.getPosition().getX(), 0);
+		assertEquals("Failure", 100, testBot.getPosition().getY(), 0);
 	}
 
 	@Test
 	public void testSetPosition() {
-		fail("Not yet implemented");
+		testBot.setPosition(position2);
+		assertEquals("Failure", 200, testBot.getPosition().getX(),0);
+		assertEquals("Failure", 200, testBot.getPosition().getY(),0);
 	}
 
 }
