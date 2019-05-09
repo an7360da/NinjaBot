8bc1e7b6b72d16dcf0702fd1dbf6e850f748551e
Jonathan Schoultz <jonis@schoultz.nu>
2019-05-09 16:24:38 +0200

    comments and formatting

--- src/info/Scan.java
+++ src/info/Scan.java
@@ -1,4 +1,5 @@
 package info;
+
 import java.awt.Color;
 import java.util.ArrayList;
 
@@ -7,66 +8,63 @@ import robocode.RobotDeathEvent;
 import robocode.ScannedRobotEvent;
 
 public class Scan {
-	
-	
-	public EnemyRobot onScannedRobot(ScannedRobotEvent event, double headingRadians){
- 				
-		EnemyRobot en= new EnemyRobot();
-		int j=0;
- 		
-			
-		if(!Environment.enemies.isEmpty()) {
-		for (int i=0; i < Environment.enemies.size(); i++) {
-			
-			if(Environment.enemies.get(i).getName().equals(event.getName())) {
-				
-				en=Environment.enemies.get(i);
-			 
-			} else {
-				j++;
+
+	public EnemyRobot onScannedRobot(ScannedRobotEvent event, double headingRadians) {
+
+		EnemyRobot en = new EnemyRobot();
+		int j = 0;
+
+		if (!Environment.enemies.isEmpty()) {
+			for (int i = 0; i < Environment.enemies.size(); i++) {
+
+				if (Environment.enemies.get(i).getName().equals(event.getName())) {
+
+					en = Environment.enemies.get(i);
+
+				} else {
+					j++;
+				}
+
 			}
-			
 		}
-		}
-			
-		if(j==Environment.enemies.size()) {
+
+		if (j == Environment.enemies.size()) {
 			Environment.enemies.add(en);
 		}
-		
+
 		en.setName(event.getName());
 		en.setEnergy((double) event.getEnergy());
-		en.setPosition(Calculations.calcPoint(Robot.getPos(), event.getDistance(), headingRadians + event.getBearingRadians()));
+		en.setPosition(Calculations.calcPoint(Robot.getPos(), event.getDistance(),
+				headingRadians + event.getBearingRadians()));
 		en.setDistance(event.getDistance());
 		en.setHeading(headingRadians);
 		en.setVelocity(event.getVelocity());
-		if(en.getEnergy()>0) {
+		if (en.getEnergy() > 0) {
 			en.setAlive(true);
-		} else{
+		} else {
 			en.setAlive(false);
-			
+
 		}
-		
-		
-	
-		//if(en == null){
-			//en = new EnemyRobot();
-			//Environment.enemies.put(event.getName(), en);
-		//}
-		
+
+		// if(en == null){
+		// en = new EnemyRobot();
+		// Environment.enemies.put(event.getName(), en);
+		// }
+
 		// här låg set-grejerna
- 
-		// normal target selection: the one closer to you is the most dangerous so attack him
-			if (Robot.hasTarget()) {
-			if(!Robot.getTarget().getAlive() || event.getDistance() < Robot.getPos().distance(Robot.getTarget().getPosition())) {
+
+		// normal target selection: the one closer to you is the most dangerous so
+		// attack him
+		if (Robot.hasTarget()) {
+			if (!Robot.getTarget().getAlive()
+					|| event.getDistance() < Robot.getPos().distance(Robot.getTarget().getPosition())) {
 				Robot.setTarget(en);
 			}
 		} else {
 			Robot.setTarget(en);
 		}
-		
+
 		return en;
 	}
-	
-	
-}
 
+}
