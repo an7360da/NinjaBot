7f68efdd616e29db8e98b26eeef22b682857862f
Rebecca Liss <rebeccaliss@Rebeccas-MacBook-Air.local>
2019-05-13 13:53:41 +0200

    javadoc

--- src/info/Environment.java
+++ src/info/Environment.java
@@ -1,9 +1,13 @@
 package info;
 
 import java.util.ArrayList;
-
+/**
+ * A class Environment that describes the different ArrayList lists of friends and enemies of NinjaBot
+ */
 public class Environment {
-	
+	/**
+	 * The ArrayLists of friends and enemies for NinjaBot
+	 */
 	public static ArrayList<EnemyRobot> enemies = new ArrayList<EnemyRobot>();
 	public static ArrayList<EnemyRobot> friends = new ArrayList<EnemyRobot>();
 	
